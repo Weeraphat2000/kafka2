4d984aa5a138336a1669e5158f5849cbc6b63571
Weeraphat2000 <weeraphat.main@gmail.com>
2024-12-18 09:40:46 +0700

    update time to do

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -26,46 +26,60 @@ export class AppController {
   @EventPattern('cat_created')
   async handleCatCreated(data: Cat) {
     log('Cat created');
-    log(data);
+    log('time to save', new Date());
+    log('data received', data);
     const cat = new this.catModel(data);
+    log('cat', cat);
     await cat.save();
+    log('cat saved', new Date());
   }
 
   @EventPattern('cat_deleted')
   async handleCatDeleted(id: string) {
     log('Cat deleted');
-    log(id);
+    log('time to delete', new Date());
+    log('received id', id);
+
     if (!isValidObjectId(id)) {
+      log('Invalid id', id);
       return 'Invalid id';
     }
 
     const cat = await this.catModel.findById(id);
     if (!cat) {
+      log('Cat not found', id);
       return 'Cat not found';
     }
 
     if (cat) {
+      log('Cat found', cat);
       await cat.deleteOne();
+      log('time to delete', new Date());
     }
   }
 
   @EventPattern('cat_updated')
   async handleCatUpdated(data: UpdateCatDto) {
     log('Cat updated');
-    log(data);
+    log('time to update', new Date());
+    log('data received', data);
     if (!isValidObjectId(data.id)) {
+      log('Invalid id', data.id);
       return 'Invalid id';
     }
 
     const cat = await this.catModel.findById(data.id);
     if (!cat) {
+      log('Cat not found', data.id);
       return 'Cat not found';
     }
 
     const { id, ...body } = data;
     log(id);
 
+    log('Cat found', cat);
     cat.set(body);
     await cat.save();
+    log('time to update', new Date());
   }
 }
